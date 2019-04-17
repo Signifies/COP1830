59b1dcdfdc84eae7de3ae8419658e464edede8bc
ES1 <CHC202@labs.ircc.cc.fl.us>
2019-04-17 10:46:38 -0400

    Finished class project code

---
+++ M6A1/test/test.js
@@ -0,0 +1,7 @@
+function browserTest() {
+//Lol im stupid I spelled it wrong.
+document.getElementById("heading").innerHTML = "Testing browser ";
+  //a href class button onclick="methodName()"
+}
+//for the canvas: html5 canvas on w3shools. Cut the draw image section into a function
+//Hight width. The image must be in the folder. Image id is in the function.
